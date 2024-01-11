e50372d9370afbfe2a84e61d7938046a0ee15ae0
Edgar Butwilowski <edgar.butwilowski@win.ch>
2024-01-11 08:50:59 +0100

    Tagged version 2028.01.08

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -17,7 +17,7 @@
  export class WelcomeComponent implements OnInit {
  
    userService: UserService;
-   appVersion: string = "2024.01.08";
+   appVersion: string = "2024.01.11";
  
    constructor(userService: UserService) {
      this.userService = userService;
